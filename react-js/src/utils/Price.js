716199c14fbb0b5ef2c1952a401fbdd4a881d5bf
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-11 22:54:36 +0100

    Add min and max functions

---
+++ react-js/src/utils/Price.js
@@ -0,0 +1,33 @@
+function CalculateGrossPrice(netPrice, tax) {
+  const percentage = (tax / 100) + 1;
+  const total = Math.round(netPrice * percentage);
+  return total;
+}
+
+function GetMaxPrice(articles) {
+  let max = 0;
+  if(articles.length < 1) return max;
+
+  for (const article of articles) {
+    const { pris, momssats } = article;
+    const grossPrice = CalculateGrossPrice(pris, momssats);
+    if(grossPrice > max) max = grossPrice;
+  }
+
+  return max;
+}
+
+function GetMinPrice(articles) {
+  let min = 0;
+  if(articles.length < 1) return min;
+
+  for (const article of articles) {
+    const { pris, momssats } = article;
+    const grossPrice = CalculateGrossPrice(pris, momssats);
+    if(grossPrice < min) min = grossPrice;
+  }
+
+  return min;
+}
+
+export { CalculateGrossPrice, GetMaxPrice, GetMinPrice }
