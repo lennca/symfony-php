c2ffacfd3e617b36c49eca4ffde94c27540357f0
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-11 17:37:22 +0100

    Add React hook to fetch data and sort response

--- react-js/src/components/IndexPage.js
+++ react-js/src/components/IndexPage.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchData } from '../services/APIService';
+import { Sort } from './utils/Sort';
 
 function IndexPage(props) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchDataFromAPI() {
+      try {
+        const { data: { products } = [] } = await fetchData()
+        const sortedProducts = Sort(products, 'artiklar_benamning')
+        sortedProducts.map((prod) => console.log(prod.artiklar_benamning))
+        setProducts(products)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    fetchDataFromAPI()
+  }, [])
+
   return (
     <div>
       <h1>Arbetsprov Montania</h1>
