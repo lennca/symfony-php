e9c9bdea2876e306fbdafe80c5783ae7f5864463
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-11 13:27:38 +0100

    Change api url to be an environment variable

--- src/Service/APIService.php
+++ src/Service/APIService.php
@@ -2,12 +2,24 @@
 
 namespace App\Service;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class APIService {
   private $httpClient;
+  private $API_URL = "";
 
-  public function __construct(HttpClientInterface $httpClient) {
+  public function __construct(HttpClientInterface $httpClient, string $projectDir) {
     $this->httpClient = $httpClient;
+
+    $dotenv = new Dotenv();
+    $dotenv->load($projectDir . '/.env');
+
+    if(isset($_ENV["API_URL"]) && $_ENV["API_URL"]) {
+      $this->API_URL = $_ENV["API_URL"];
+    } else {
+      throw new Exception("Please provide the api url in the .env file");
+    }
   }
 
   /**
@@ -18,7 +30,7 @@ class APIService {
   public function fetchProducts() {
     $response = $this->httpClient->request(
       'GET',
-      'https://dev14.ageraehandel.se/sv/api/product'
+      $this->API_URL
     );
 
     $content = $response->getContent();
