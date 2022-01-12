edfb53a80e1c9f35c243fd0d3e5cdc83df6df020
Caesar Lennartsson <caesar.1999@hotmail.com>
2022-01-12 11:12:55 +0100

    Update non used App component

--- react-js/src/components/Kategori.js
+++ react-js/src/components/Kategori.js
@@ -4,7 +4,7 @@ import Artikel from './Artikel';
 function Kategori({ kategori }) {
   return (
     <div>
-      <h5>{kategori.category}</h5>
+      <h5>{kategori.category.toUpperCase()}</h5>
       {kategori.products.map((produkt) => {
         return <Artikel produkt={produkt} key={produkt.id} />
       })}
